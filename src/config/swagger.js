04e2b17ededffce491ae89affa3938f0bafb165d
iamkuldeep07 <kuldeep45.kd@gmail.com>
2026-04-28 16:50:57 +0530

    swagger fix

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -15,8 +15,8 @@ const options = {
     },
     servers: [
       {
-        url: `http://localhost:${process.env.PORT || 3000}`,
-        description: 'Development server',
+        url: `https://school-management-it4o.onrender.com`,
+        description: 'Production server',
       },
     ],
     components: {
